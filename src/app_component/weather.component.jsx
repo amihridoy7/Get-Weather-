bd06bf26fd3547759c582d415f303cc7b410a803
amihridoy7 <amihridoy55@gmail.com>
2019-12-02 12:29:48 +0600

    Get Weather added

---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+import Weather from './app_component/weather.component';
+
+const weather = () => {
+    return (
+        <div className = "container">
+            <Weather/>
+        </div>
+    );
+}
+
+export default App;
